2a3e6ffe5435ec3f462a496eb1d59e5cc80cb8bc
mayo.ogawa <smayoj@gmail.com>
2020-07-20 02:51:56 +0000

    picture none

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -2,7 +2,6 @@ class Anime < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
-  mount_uploader :image, ImageUploader
   
   has_one_attached :image
   
